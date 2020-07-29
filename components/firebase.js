d987f21f78f44fa2d093b300afb82840cdfb47c5
Gim kelum <49275770+gim96@users.noreply.github.com>
2020-07-29 17:06:32 +0530

    quiz completed......!

--- components/firebase.js
+++ components/firebase.js
@@ -1,15 +1,7 @@
 import * as firebase from "firebase";
 import firestore from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBAr62NNmGQwswgSHssuAmIU42llb4uu_0",
-  authDomain: "mypro-9a838.firebaseapp.com",
-  databaseURL: "https://mypro-9a838.firebaseio.com",
-  projectId: "mypro-9a838",
-  storageBucket: "mypro-9a838.appspot.com",
-  messagingSenderId: "1046736683722",
-  appId: "1:1046736683722:web:b1a70496dd086898be2a33",
-};
+const firebaseConfig = {};
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore();
